cac223c6ceccf09b97a300c15917270607fdd324
lilmeov <nagima242424@gmail.com>
2022-01-27 01:31:54 +0600

    Connected entities to db

--- src/main/java/com/attractor/futureStore/product/Product.java
+++ src/main/java/com/attractor/futureStore/product/Product.java
@@ -1,5 +1,26 @@
 package com.attractor.futureStore.product;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "products")
 public class Product {
-    
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  @Column(length = 100)
+  private String name;
+  @Column(length = 123)
+  private String image;
+  private int amount;
+  @Column(length = 128)
+  private String description;
+  private float price;
+  @ManyToOne
+  private ProductType productType;
+
+
 }
--- src/main/java/com/attractor/futureStore/product/ProductType.java
+++ src/main/java/com/attractor/futureStore/product/ProductType.java
@@ -1,4 +1,16 @@
 package com.attractor.futureStore.product;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "product_types")
 public class ProductType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 100)
+    private String type;
 }
